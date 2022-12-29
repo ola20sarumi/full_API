390c628011849e0be2a3111532d16ad074396d32
ola20sarumi <61728654+ola20sarumi@users.noreply.github.com>
2022-12-29 23:19:41 +0200

    Web API for the full_ui

---
+++ full.API/Controllers/EmployeesController.cs
@@ -0,0 +1,48 @@
+﻿using full.API.Data;
+using full.API.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace full.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EmployeesController : Controller
+    {
+        private readonly FullDbContext _fullDbContext;
+        public EmployeesController(FullDbContext fullDbContext)
+        {
+            _fullDbContext = fullDbContext;
+        }
+        [HttpGet]
+        public async Task<ActionResult> GetAllEmployees()
+        {
+          var employees = await _fullDbContext.Employees.ToListAsync();
+            return Ok(employees);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddEmployee([FromBody] Employee employeeRequest)
+        {
+            employeeRequest.Id = Guid.NewGuid();
+           await _fullDbContext.Employees.AddAsync(employeeRequest);
+            await _fullDbContext.SaveChangesAsync();
+            return Ok(employeeRequest);
+        }
+
+        [HttpGet]
+        [Route("(id:Guid)")]
+        public async Task<IActionResult> GetEmployee([FromRoute] Guid id)
+        {
+            var employee = 
+                await _fullDbContext.Employees.FirstOrDefaultAsync(x => x.Id == id);
+
+            if (employee == null)
+            {
+                return NotFound();
+            }
+            return Ok(employee);
+
+        }
+    }
+}
---
+++ full.API/Data/FullDbContext.cs
@@ -0,0 +1,14 @@
+﻿using full.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace full.API.Data
+{
+    public class FullDbContext : DbContext
+    {
+        public FullDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
